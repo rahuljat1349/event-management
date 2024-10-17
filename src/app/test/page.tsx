9a887a805b8668e0bb7c08b740e72f58f2bec973
Rahul <rahul@MBA16GB.local>
2024-10-17 16:08:50 +0530

    Eslint Updated

---
+++ src/app/test/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { useState } from 'react'
+
+const Page = () => {
+  const [test, setTest] = useState('tesde')
+  const click = () => {
+    setTest('Test')
+  }
+  return <div onClick={click}>{test}</div>
+}
+
+export default Page
